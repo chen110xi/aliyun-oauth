c6d580d639402d4533ae8c6309b7471a1b05e7b1
Jackson Tian <shyvo1987@gmail.com>
2015-09-08 10:15:58 +0800

    Add check_accesstoken api

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var util = require('util');
 
 var OAuth = require('./lib/oauth');
 util._extend(OAuth.prototype, require('./lib/openapi'));
+util._extend(OAuth.prototype, require('./lib/innerapi'));
 
 module.exports = OAuth;
